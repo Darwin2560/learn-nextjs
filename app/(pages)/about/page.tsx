e5fe4bf76e4917565d2412deabf9ec719f8818da
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-23 20:44:10 +0100

    feat: nested layout

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'About Page',
+    description: 'SEO Description',
+    keywords: ['Información', 'Darwin', 'About Page'],
+}
+
+export default function About() {
+    return (
+        <span className="text-5xl">Pricing Page</span>
+    )
+}
