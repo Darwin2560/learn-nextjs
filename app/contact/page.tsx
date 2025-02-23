dc759017daf4dd150e9f9c8e9eec7243f38c765b
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-23 19:18:03 +0100

    feat: additional routes

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Contact Page',
+    description: 'SEO Description',
+    keywords: ['Información', 'Darwin', 'Contact Page'],
+}
+
+export default function Contact() {
+    return (
+        <span>Contact Page</span>
+    )
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Pricing Page',
+    description: 'SEO Description',
+    keywords: ['Información', 'Darwin', 'Pricing Page'],
+}
+
+export default function Pricing() {
+    return (
+        <span>Pricing Page</span>
+    )
+}
